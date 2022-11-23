47b4cfb09db953eeb48497bfcc8f3f02c27f8397
vmelo.gon@gmail.com <vmelo.gon@gmail.com>
2022-11-23 16:02:39 -0300

    Remove funções para o programa e deixa apenas as funções read_bake_file e prompt_echo; mudanças na tar de bake para facilitar o entendimento do script.

--- bake.sh
+++ bake.sh
@@ -6,9 +6,12 @@
   Root dir can be set with 'bake set'.
 DESC
 
+# TODO: 'bake where': read a dir and writes to that dir once
+# TODO: 'bake n': 'n' is interval in minutes for creating backups
+
 readonly bake_file=$HOME/.bake
 
-prompt_echo()
+prompt_echo ()
 {
   local options
   local args
@@ -23,9 +26,8 @@ prompt_echo()
   echo $options $(basename $0): $args
 }
 
-read_bake_file()
+read_bake_file ()
 {
-
   if [ -s $bake_file ]; then
     . $bake_file 2>/dev/null
     if [ ! $? -eq 0 ] || [ ! -d $root_dir ]; then
@@ -44,85 +46,47 @@ read_bake_file()
       done
     fi
   else
-    touch $bake_file
-    echo 'root_dir=' >> $bake_file
+    echo 'root_dir=' > $bake_file
     . $bake_file 2>/dev/null
   fi
 }
 
-bake ()
-{
-
-  if [ -z $root_dir ]; then 
-    local root_dir
-    root_dir=$(pwd)
-  fi
-
-  if [ $root_dir != $(pwd) ]; then 
-    cd ${root_dir}
-  fi
-
-  cd ..
-
-  local base_name=$(basename $root_dir)
-
-  # /foo/bar/ -> foo_bar.1669158229.tar.gz
-  # alternative would be 'root_dir=${root_dir//\//_}' and passing '${root_dir:1}' to sed - using UNIX commands for POSIX compliance
-  local bak_file=/tmp/$(echo $root_dir | tr / _ | cut -c 2-).$(date +%s).tar.gz
-
-  # '--checkpoint=.500' - show progress of tar cmd
-  # https://www.gnu.org/software/tar/manual/tar.html#checkpoints
-  tar -zcf $bak_file --totals --checkpoint=.500 $base_name
-
-  if [ $? -eq 0 ]; then  
-    prompt_echo "file created: $bak_file"
-    exit 0
-  fi
-}
-
-bake_set ()
-{
-  local root_dir
-  root_dir=$(pwd)
-  sed -i "s|\(root_dir=\)\(.*\)|\1${root_dir}|" $bake_file || exit 1
-  prompt_echo "root_dir=$root_dir" && exit 0
-}
-
-bake_unset ()
-{
-  sed -i "s/\(root_dir=\)\(.*\)/\1/" $bake_file || exit 1
-  read_bake_file
-  prompt_echo "root_dir=$root_dir" && exit 0
-}
-
-bake_what ()
-{
-  prompt_echo "root_dir=$root_dir" && exit 0
-}
-
-# TODO: 'bake where': read a dir and writes to that dir once
-
-# TODO: 'bake n': 'n' is interval in minutes for creating backups
-
-[ $# -gt 1 ] && exit 1
+if [ $# -gt 1 ]; then
+  prompt_echo 'bake [set|unset|what]'
+  exit 1
+fi
 
 read_bake_file
 
 case "$1" in
   '')
-    bake
-    exit 1
+    [ -z $root_dir ] && root_dir=$(pwd)
+    [ $root_dir != $(pwd) ] && cd ${root_dir}
+    cd ..
+    bak_file=/tmp/$(echo $root_dir | tr / _ | cut -c 2-).$(date +%s).tar.gz
+    # '--checkpoint=.500' - show progress
+    tar -zc --totals --checkpoint=.500 $(basename $root_dir) > $bak_file
+    if [ $? -eq 0 ]; then  
+      prompt_echo "file created: $bak_file"
+      exit 0
+    else
+      exit 1
+    fi
     ;;
   set)
-    bake_set
+    root_dir=$(pwd)
+    sed -i "s|\(root_dir=\)\(.*\)|\1${root_dir}|" $bake_file || exit 1
+    prompt_echo "root_dir=$root_dir" && exit 0
     exit 1
     ;;
   unset)
-    bake_unset
+    sed -i "s/\(root_dir=\)\(.*\)/\1/" $bake_file || exit 1
+    read_bake_file
+    prompt_echo "root_dir=$root_dir" && exit 0
     exit 1
     ;;
   what)
-    bake_what
+    prompt_echo "root_dir=$root_dir" && exit 0
     exit 1
     ;;
   *)
